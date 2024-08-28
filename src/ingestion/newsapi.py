679f52bd27db2e1ccd400e5c36ed0921f89abf00
Devasy Patel <110348311+Devasy23@users.noreply.github.com>
2024-08-28 15:06:17 +0530

    feat: added files to support fetching news from newsapi (#1)
    * feat: added files to support fetching news from newsapi
    
    * made relative import changes
    
    * black and isort
    
    * added precommit-hooks, and updated requirements.txt
    
    * Integrated review comments
    
    * Update .pre-commit-config.yaml

---
+++ src/ingestion/newsapi.py
@@ -0,0 +1,55 @@
+import json
+import os
+from datetime import datetime
+import requests
+from dotenv import load_dotenv
+
+from src.utils.logger import setup_logger
+
+# Load API key from .env file
+load_dotenv()
+API_KEY = os.getenv("NEWS_API_KEY")
+
+# Configure logger
+logger = setup_logger()
+
+
+def fetch_news(query, from_date: datetime, to_json=True):
+    url = f"https://newsapi.org/v2/everything?q={query}&from={from_date}&sortBy=popularity&apiKey={API_KEY}"
+    try:
+        logger.debug("Requesting data from NewsAPI")
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an error for bad status codes
+        data = response.json()
+
+        if data.get("status") == "ok":
+            logger.info(f"Total results: {data.get('totalResults')}")
+            if to_json:
+                try:
+                    filename = f"{query.replace(' ', '_')}_{from_date}.json"
+                    with open(filename, "w", encoding="utf-8") as f:
+                        json.dump(data, f, ensure_ascii=False, indent=4)
+                    logger.info(f"Results stored in {filename}")
+                except Exception as e:
+                    logger.error(f"Error occurred while storing results: {str(e)}")
+            else:
+                for article in data.get("articles", []):
+                    logger.debug(f"Article Title: {article.get('title')}")
+                    try:
+                        logger.debug(
+                            f"Article Description: {article.get('description')}"
+                        )
+                    except UnicodeEncodeError:
+                        logger.debug(
+                            f"Article Description: {article.get('description').encode('utf-8').decode('ascii', 'ignore')}"
+                        )
+        else:
+            logger.error(f"Error in response: {data}")
+    except requests.exceptions.RequestException as e:
+        logger.error(f"HTTP Request failed: {type(e).__name__} - {str(e)}")
+
+
+if __name__ == "__main__":
+    # if __name__ == "__main__" and __package__ is None:
+    __package__ = "src.ingestion"
+    fetch_news(query="Kolkata Murder case", from_date="2024-08-21")
---
+++ src/utils/logger.py
@@ -0,0 +1,42 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
+import colorlog
+
+
+def setup_logger(log_file='app.log'):
+    logger = logging.getLogger("news_api_logger")
+    logger.setLevel(logging.DEBUG)
+
+    # Create console handler with color coding
+    handler = colorlog.StreamHandler()
+    handler.setLevel(logging.DEBUG)
+
+    formatter = colorlog.ColoredFormatter(
+        "%(log_color)s%(levelname)s%(reset)s: [%(asctime)s] %(filename)s:%(funcName)s:%(lineno)d - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        log_colors={
+            "DEBUG": "cyan",
+            "INFO": "green",
+            "WARNING": "yellow",
+            "ERROR": "red",
+            "CRITICAL": "red,bg_white",
+        },
+    )
+    handler.setFormatter(formatter)
+
+    # Create rotating file handler
+    file_handler = RotatingFileHandler(log_file, maxBytes=5000000, backupCount=5)
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(
+        logging.Formatter(
+            "%(levelname)s: [%(asctime)s] %(filename)s:%(funcName)s:%(lineno)d - %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+        )
+    )
+
+    # Add handlers to the logger
+    logger.addHandler(handler)
+    logger.addHandler(file_handler)
+
+    return logger
